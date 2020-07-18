b497ae7a31d048624a0d193e078d7a61843540b8
miyamoen <akihito.terazawa@siiibo.com>
2020-07-18 19:00:01 +0900

    Main module 外向けの関数を入れる予定だがまだやってない

---
+++ src/Origami.elm
@@ -0,0 +1,68 @@
+module Origami exposing (Style, batch, property, qt)
+
+{-| -}
+
+import Origami.Css.Block as Block exposing (Property(..))
+import Origami.Css.Style as Style
+
+
+{-| -}
+type alias Style =
+    Style.Style
+
+
+{-| For use with [`font-family`](https://developer.mozilla.org/docs/Web/CSS/font-family)
+
+    fontFamily serif
+
+    fontFamilies [ qt "Gill Sans Extrabold", "Helvetica", .value sansSerif ]
+
+-}
+qt : String -> String
+qt str =
+    "\"" ++ str ++ "\""
+
+
+{-| Create a style from multiple other styles.
+
+    underlineOnHover =
+        batch
+            [ textDecoration none
+
+            , hover
+                [ textDecoration underline ]
+            ]
+
+    css
+        [ color (rgb 128 64 32)
+        , underlineOnHover
+        ]
+
+...has the same result as:
+
+    css
+        [ color (rgb 128 64 32)
+        , textDecoration none
+        , hover
+            [ textDecoration underline ]
+        ]
+
+-}
+batch : List Style -> Style
+batch =
+    Style.BatchStyles
+
+
+{-| Define a custom property.
+
+    css [ property "-webkit-font-smoothing" "none" ]
+
+...outputs
+
+    -webkit-font-smoothing: none;
+
+-}
+property : String -> String -> Style
+property key value =
+    Property key value
+        |> Style.PropertyStyle
